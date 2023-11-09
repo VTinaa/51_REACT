53d0cf0c094eab357c9deb99152c25f751cf140b
valentine <yecvhuk.valentine@gmaiil.com>
2023-11-09 17:07:27 +0200

    task_home

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,16 @@
+import './footer.css'
+import FooterItem from '../FooterItem';
+
 const Footer = () => {
     return ( 
-    <div>
-        This is Footer component
+    // <div>
+    //     This is Footer component
+    // </div>
+    <div className='common-footer'>
+        <FooterItem text='First elem' isLowercaseText={true} />
+        <FooterItem text='Second elem' isLowercaseText={false} />
+        <FooterItem text='Third elem' isLowercaseText={false} />
+        <FooterItem text='Fourth elem' isLowercaseText={true} />
     </div>
     );
 }
---
+++ src/components/FooterItem/FooterItem.jsx
@@ -0,0 +1,19 @@
+const FooterItem = (props) => {
+
+    const { text, isLowercaseText} = props;
+    console.log(props);
+
+    let textToRender;
+    
+    if (isLowercaseText) {
+        textToRender = text.toLowerCase()
+    }
+    else {
+        textToRender = text.toUpperCase()
+    }
+    return (
+        <div>{textToRender}</div>
+    )
+}
+
+export default FooterItem;
