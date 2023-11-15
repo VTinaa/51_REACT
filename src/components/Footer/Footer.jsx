f4ec9f0cbba0f2fbe24db4c86387dc864103ce0d
valentine <yecvhuk.valentine@gmaiil.com>
2023-11-16 01:23:21 +0200

    task correction_footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,16 +2,18 @@ import './footer.css'
 import FooterItem from '../FooterItem';
 
 const Footer = () => {
-    return ( 
-    // <div>
-    //     This is Footer component
-    // </div>
-    <div className='common-footer'>
-        <FooterItem text='First elem' isLowercaseText={true} />
-        <FooterItem text='Second elem' isLowercaseText={false} />
-        <FooterItem text='Third elem' isLowercaseText={false} />
-        <FooterItem text='Fourth elem' isLowercaseText={true} />
-    </div>
+    return (
+
+        <footer>
+            <FooterItem text='footer' isLowercaseText={true} />
+            <div className='common-footer'>
+                <FooterItem text='First elem' isLowercaseText={true} />
+                <FooterItem text='Second elem' isLowercaseText={true} />
+                <FooterItem text='Third elem' isLowercaseText={false} />
+                <FooterItem text='Fourth elem' isLowercaseText={false} />
+            </div>
+        </footer>
+
     );
 }
 
--- src/components/FooterItem/FooterItem.jsx
+++ src/components/FooterItem/FooterItem.jsx
@@ -3,16 +3,15 @@ const FooterItem = (props) => {
     const { text, isLowercaseText} = props;
     // console.log(props);
 
-    let textToRender;
+    // let textToRender;
+    // isLowercaseText ? textToRender = text.toLowerCase() : textToRender = text.toUpperCase();
     
-    if (isLowercaseText) {
-        textToRender = text.toLowerCase()
-    }
-    else {
-        textToRender = text.toUpperCase()
-    }
-    return (
-        <div>{textToRender}</div>
+    let textToRender =  isLowercaseText ? text.toLowerCase() : text.toUpperCase();
+
+    return(
+        <div>
+            {textToRender}
+        </div>
     )
 }
 
